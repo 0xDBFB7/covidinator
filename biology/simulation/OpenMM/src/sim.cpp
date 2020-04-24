1a9bb303dca8ce48d8fc50d6f5112da65af9abfc
BasedOnTechnology <therobotist@gmail.com>
2020-04-24 16:19:58 -0400

    OpenMM plotting

--- biology/simulation/OpenMM/src/sim.cpp
+++ biology/simulation/OpenMM/src/sim.cpp
@@ -18,32 +18,38 @@
 
 int main(){ try {
 
+    double timestep = 0.004; //picoseconds
+    double end_time = 10; //also picoseconds
 
     // Load any shared libraries containing GPU implementations.
     OpenMM::Platform::loadPluginsFromDirectory("/home/arthurdent/Programs/openmm-7.4.1/openmm-7.4.1/install/lib/plugins");
     // Create a system with nonbonded forces.
     OpenMM::System system;
-    OpenMM::VerletIntegrator integrator(0.004); // step size in ps
+    OpenMM::VerletIntegrator integrator(timestep);
 
 
-    OpenMM::NonbondedForce* lennard_jones_force = new OpenMM::NonbondedForce();
-    system.addForce(lennard_jones_force);
+    // OpenMM::NonbondedForce* lennard_jones_force = new OpenMM::NonbondedForce();
+    // system.addForce(lennard_jones_force);
 
 
-    OpenMM::CustomExternalForce* force = new OpenMM::CustomExternalForce("k*((x-x0)^2+(y-y0)^2+(z-z0)^2)");
-    force->addGlobalParameter("k", 100.0);
-    force->addPerParticleParameter("x0");
+    //the force is specified by a potential energy function
+
+    OpenMM::CustomExternalForce* electric_force = new OpenMM::CustomExternalForce("Ex*charge");
+    electric_force->addGlobalParameter("Ex", 100.0);
+    electric_force->addPerParticleParameter("charge");
 
     // Create three atoms.
-    std::vector<OpenMM::Vec3> initial_position(3);
-    for (int a = 0; a < 3; ++a)
+    std::vector<OpenMM::Vec3> initial_position(1);
+    for (int a = 0; a < 1; ++a)
     {
         initial_position[a] = OpenMM::Vec3(0.5*a,0,0); // location, nm
 
         system.addParticle(39.95); // mass of Ar, grams per mole
 
+        electric_force.addParticle();
+
         // charge, L-J sigma (nm), well depth (kJ)
-        lennard_jones_force->addParticle(0.0, 0.3350, 0.996); // vdWRad(Ar)=.188 nm
+        // lennard_jones_force->addParticle(0.0, 0.3350, 0.996); // vdWRad(Ar)=.188 nm
     }
 
 
@@ -54,21 +60,49 @@ int main(){ try {
 
     context.setPositions(initial_position);
 
+    std::vector<double> x_position;
+    std::vector<double> x_force;
+
     // Simulate.
     for (int iteration=0; ;iteration++) {
         // Output current state information.
-        OpenMM::State state    = context.getState(OpenMM::State::Positions);
-        const double  timeInPs = state.getTime();
+        OpenMM::State state    = context.getState(OpenMM::State::Positions | OpenMM::State::Forces);
+
         dump_to_xyz_file("output", iteration, state);
 
+        const std::vector<OpenMM::Vec3>& positions = state.getPositions();
+        x_position.push_back(positions[0][X]);
+        const std::vector<OpenMM::Vec3>& forces = state.getForces();
+        x_force.push_back(forces[0][X]);
 
-        if (timeInPs >= 10.)
-            break;
+
+        const double current_time = state.getTime();
+        if (current_time >= end_time) break;
 
         // Advance state many steps at a time, for efficient use of OpenMM.
         integrator.step(10); // (use a lot more than this normally)
     }
 
+    mglGraph gr;
+    mglData x_pos_mgl;
+    mglData x_force_mgl;
+
+    x_pos_mgl.Link(x_position.data(),x_position.size());
+    x_force_mgl.Link(x_force.data(),x_force.size());
+
+    gr.SubPlot(1,2,0);
+    gr.Box();
+    gr.SetRange('y',*min_element(x_position.begin(), x_position.end()),*max_element(x_position.begin(), x_position.end()));
+    gr.Plot(x_pos_mgl,"Y");
+    gr.Axis();
+
+    gr.SubPlot(1,2,1);
+    gr.Box();
+    gr.SetRange('y',*min_element(x_force.begin(), x_force.end()),*max_element(x_force.begin(), x_force.end()));
+    gr.Plot(x_force_mgl,"R");
+    gr.Axis();
+
+    gr.WriteFrame("output.png");
 
 
 
--- biology/simulation/OpenMM/src/sim.hpp
+++ biology/simulation/OpenMM/src/sim.hpp
@@ -4,16 +4,16 @@
 #include "OpenMM.h"
 #include <cstdio>
 #include <iostream>
-// #include <math.h>
+#include <math.h>
 #include <vector>
-// #include <numeric>
-// #include <limits>
-// #include <fstream>
-// #include <assert.h>
-// #include <mgl2/mgl.h>
-// #include <algorithm>
-// #include <chrono>
-// #include <fenv.h>
+#include <numeric>
+#include <limits>
+#include <fstream>
+#include <assert.h>
+#include <mgl2/mgl.h>
+#include <algorithm>
+#include <chrono>
+#include <fenv.h>
 
 #include <helpers.hpp>
 
