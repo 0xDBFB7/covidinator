7d349a59c7c138fe75d67e5fb8fc83583f4b8382
BasedOnTechnology <therobotist@gmail.com>
2020-04-30 14:24:23 -0400

    Background subtraction

--- software/sweep.py
+++ software/sweep.py
@@ -12,8 +12,9 @@ def run_sweep(start_freq, end_freq, bin_width, gain_db, samples_per_freq_multipl
     gain_db: 0-40 dB. LNA and VGA are adjusted equally.
     samples_per_freq_multiplier:
 
+    hackrf_sweep doesn't seem to be particularly deterministic;
+    frequency ranges can be left out, the start of a sweep can happen anywhere, etc.
     '''
-
     N_points = int(((end_freq-start_freq)*1e6)/bin_width)
 
     samples_per_freq = 8192*(2**samples_per_freq_multiplier)
@@ -22,38 +23,29 @@ def run_sweep(start_freq, end_freq, bin_width, gain_db, samples_per_freq_multipl
     #two sweeps are generally required to fill the entire spectrum.
 
 
-    freqs = np.arange(start_freq*1e6, end_freq*1e6, bin_width)
-    data = np.zeros(N_points)
-    # hackrf_sweep seems not to be entirely deterministic.
-    hit = np.zeros(N_points)
+    freqs = np.zeros(N_sweeps*N_points)
+    data = np.zeros(N_sweeps*N_points)
 
 
     lna_gain = 0 + int(float(gain_db)/8.0)*8
     vga_gain = 0 + int(float((gain_db/40.0)*62.0)/2.0)*2
-    command = 'hackrf_sweep -f {0}:{1} -a 0 -n{2} -w{3} -l {4} -g {5} -N 100'.format(\
-                        start_freq, end_freq, samples_per_freq, bin_width, lna_gain, vga_gain)
+    command = 'hackrf_sweep -f {0}:{1} -a 0 -n{2} -w{3} -l {4} -g {5} -N{6}'.format(\
+                        start_freq, end_freq, samples_per_freq, bin_width, lna_gain, vga_gain, N_sweeps)
     p = Popen(command, stdout = PIPE, shell = True)
 
+    n = 0
     for line in p.stdout:
         try:
             new_input = np.array([float(i) for i in line.decode().split(',')[2:]])
             for i in range(0, (len(new_input))-4):
                 freq = (new_input[0] + ((new_input[1] - new_input[0]) / (len(new_input)-4))*i) + LO_freq
-                index = int((freq - start_freq) / bin_width)
-                if(index >= N_points):
-                    continue
-                data[index] = new_input[i+4]
-                # freqs[index] = freq #lines are often out of order.
-                hit[index] = 1
+                data[n] = new_input[i+4]
+                freqs[n] = freq #lines are often out of order.
+                n+=1
         except Exception as e:
             print(e)
             print(line)
             pass
-
-        if(np.all(hit == 1)):
-            p.terminate()
-            print("Sweep done, all data collected.")
-            break
 
     data = data[freqs.argsort()]
     freqs = freqs[freqs.argsort()]
@@ -68,25 +60,41 @@ def run_sweep(start_freq, end_freq, bin_width, gain_db, samples_per_freq_multipl
 
     return freqs, data
 
+def peak_detect(data, freqs, peak_interval=20):
+    peaks_data = np.array([max(data[i:i+peak_interval]) for i in range(0, len(data), peak_interval)])
+    peaks_freqs = [freqs[i] for i in range(0, len(data), peak_interval)]
+    return peaks_freqs, peaks_data
 
-peak_interval = 20
-#
-# averaged_data = np.zeros()
-# freqs = np.zeros()
+def take_sample(freqs, averages=1):
+    averaged_data = np.zeros(0)
 
-for i in range(0,5):
-    x, data = run_sweep(1, 6000, 10000, 30.0, 0)
-    if(not averaged_data):
-        averaged_data = data
-        freqs = x
-    else:
-        averaged_data += np.argwhere(x)
+    for i in range(0,averages):
+        x, data = run_sweep(1, 6000, 10000, 30.0, 0)
+        if(not len(averaged_data)):
+            averaged_data = data
+        if(not len(freqs)):
+            freqs = x
+        else:
+            averaged_data[np.where(np.isin(x,freqs))] += data[np.where(np.isin(x,freqs))]
 
-    # peaks_data = np.array([max(data[i:i+peak_interval]) for i in range(0, len(data), peak_interval)])
-    # peaks_freqs = [x[i] for i in range(0, len(data), peak_interval)]
+    averaged_data /= averages
 
+    return averaged_data
+
+
+
+averages = 5
+
+freqs = np.zeros(0)
+
+background = take_sample(freqs, averages=1)
+
+averaged_data = take_sample(freqs, averages=1)
+
+averaged_data -= background
+# averaged_data -= 500
 
 print(data)
 # plt.plot(x,data)
-plt.plot(peaks_freqs,peaks_data)
+plt.plot(freqs,averaged_data)
 plt.show()
