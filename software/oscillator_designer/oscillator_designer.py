de70cae46f9bc0f583d010a784c5af7300a8da85
BasedOnTechnology <therobotist@gmail.com>
2020-05-21 15:50:15 -0400

    :(

--- software/oscillator_designer/oscillator_designer.py
+++ software/oscillator_designer/oscillator_designer.py
@@ -107,11 +107,11 @@ def cost_function(x, retained_values, retained_indices, desired_center_frequency
     phase_at_peak = phase_shift[feedback_voltage_peak_indices][0]
 
     freq_coeff = 4
-    phase_coeff = 1.5
+    phase_coeff = 3
     ratio_coeff = 0.5
     insertion_loss_coeff = 0.2
 
-    frequency_cost = freq_coeff * (abs(desired_center_frequency-fb_peak_frequencies[0])/desired_center_frequency)
+    frequency_cost = freq_coeff * (abs(desired_center_frequency-fb_peak_frequencies[0])/1e9)
     phase_cost = phase_coeff * abs(1.0 - phase_at_peak)
     ratio_cost = ratio_coeff * fb_peak_ratio
     insertion_loss_cost = (1.0 - fb_peak_values[0])*insertion_loss_coeff
@@ -170,14 +170,14 @@ def optimize(bounds, initial_guess, retained_values, retained_indices, desired_f
 # could also run two sims at extremes and average retained inductance values
 
 
-num_vars = 10
+num_vars = 9
 initial_guess = [1]*num_vars
 bounds = [(0.1,10)]*num_vars
 
-bounds[3] = (0.3,2)
-bounds[4] = (0.3,2)
-bounds[6] = (0.3,2)
-bounds[7] = (0.3,2)
+bounds[3] = (0.35,2)
+bounds[4] = (0.35,2)
+bounds[6] = (0.35,2)
+bounds[7] = (0.35,2)
 
 # initial_guess[4] = 0.2
 # initial_guess[3] = 0.2
@@ -186,19 +186,24 @@ bounds[7] = (0.3,2)
 retained_values = np.array([])
 retained_indices = []
 
-frequency_sweep = [7e9,10e9]
+frequency_sweep = [10e9,7e9]
 
 ideal_values = [[]]*len(frequency_sweep)
 ideal_values[0] = initial_guess
 # ideal_values.append(initial_guess)
 
+# import cProfile
+# cProfile.run('optimize(bounds, initial_guess, retained_values, retained_indices, frequency_sweep[0]     )')
+# 28 s in load_data,
+# 53 s in qucs,
+
 for i in range(0, len(frequency_sweep)):
     ideal_values[i] = (optimize(bounds, initial_guess, retained_values, retained_indices, frequency_sweep[i]))
 
-    ideal_values[i] = np.array(np.round(ideal_values[i],1))
+    # ideal_values[i] = np.array(np.round(ideal_values[i],1))
 
     retained_values = ideal_values[i]
-    retained_indices = np.array([0,1,2,5,8,9])
+    retained_indices = np.array([0,2,4,6,8])
 
 ##Sweep all varactor values, determine points with minimum distance to ideal frequency,
 
