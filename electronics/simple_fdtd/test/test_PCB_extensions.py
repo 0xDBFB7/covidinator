5af414ec44a1c4aad3e7fca0279c212c5a3bc493
BasedOnTechnology <therobotist@gmail.com>
2020-05-31 13:42:42 -0400

    oh man, scary git issue

--- electronics/simple_fdtd/test/test_PCB_extensions.py
+++ electronics/simple_fdtd/test/test_PCB_extensions.py
@@ -19,7 +19,7 @@ def test_copper_creation():
     pcb.create_planes(0.032e-3, 6e7)
     pcb.create_substrate(0.8e-3, 4.4, 0.02, 9e9)
     pcb.construct_copper_geometry_from_svg(0.032e-3, 6e7, 'test/test.svg')
-    pcb.dump_to_vtk('test/test',0)
+    pcb.dump_to_vtk('test/dumps/test',0)
 
 
 def test_ports():
@@ -29,4 +29,4 @@ def test_ports():
     pcb.create_substrate(0.8e-3, 4.4, 0.02, 9e9)
     pcb.construct_copper_geometry_from_svg(0.032e-3, 6e7, 'test/test.svg')
     pcb.reference_port = Port(pcb, 0, X, 1.0, 10.0e-3, 10.0e-3, 1.5e-3, )
-    pcb.dump_to_vtk('test/test',0)
+    pcb.dump_to_vtk('test/dumps/test',1)
