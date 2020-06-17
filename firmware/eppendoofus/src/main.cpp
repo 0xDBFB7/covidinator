72e894829ee748fc040ca619aa76e0480a1dfb4f
BasedOnTechnology <therobotist@gmail.com>
2020-06-17 11:18:29 -0400

    More teensy

--- firmware/eppendoofus/src/main.cpp
+++ firmware/eppendoofus/src/main.cpp
@@ -1,22 +1,17 @@
-#include "WProgram.h"
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wall"
+//#include "WProgram.h"
+// #pragma GCC diagnostic pop
+
+
 
 int main(void)
 {
-
-	// To use Teensy 3.0 without Arduino, simply put your code here.
-	// For example:
-
-	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
-	}
-
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
+	// pinMode(13, OUTPUT);
+	// while (1) {
+	// 	digitalWriteFast(13, HIGH);
+	// 	delay(100);
+	// 	digitalWriteFast(13, LOW);
+	// 	delay(100);
+	// }
 }
