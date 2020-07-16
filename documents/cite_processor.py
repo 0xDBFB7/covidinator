a6f11e4ce77a0edbeda5fe88b310ed6847800751
BasedOnTechnology <therobotist@gmail.com>
2020-07-16 11:30:07 -0400

    Inserting citations

---
+++ documents/cite_processor.py
@@ -0,0 +1,17 @@
+import json
+
+#Export Better BibTeX JSON from Zotero
+
+JSON_INPUT = 'test.json'
+
+refs = json.loads(open(JSON_INPUT).read())
+collections = refs["collections"]
+# A tree of
+#
+items = refs["items"]
+#items have
+# notes: [ "<p>, ",
+
+# they can also be standalone notes, with the key "note" present.
+
+print(collections)
