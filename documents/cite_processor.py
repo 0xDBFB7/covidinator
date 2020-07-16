bbb034ed2265a966367f846d10a6cfa1c36999a1
BasedOnTechnology <therobotist@gmail.com>
2020-07-16 17:08:53 -0400

    Cite processor almost done

--- documents/cite_processor.py
+++ documents/cite_processor.py
@@ -41,41 +41,62 @@ tex_files.pop(tex_files.index("preamble.tex")) # don't include the definition of
 #         ["creators"][0]["lastName"]
 # Cite keys
 
+CITE_TAG = 'supercite{'
+COLLECT_TAG = 'supercollection{'
+
 cite_keys = [x["citekey"] for x in items if "citekey" in x.keys()]
+filtered_items = [x for x in items if "citekey" in x.keys()]
 for tex_file in tex_files:
     with open(tex_file) as f:
         tex = f.read()
     with open("backup/backup_" + tex_file, "w+") as f:
         f.write(tex)
 
-    keys = re.finditer('supercite{', tex)
-    for key in keys:
-        tag_start = key.end()
+    keys = list(re.finditer(CITE_TAG, tex))
+    # offset = 0
+    for i in range(0, len(keys)):
+        key = keys[i]
+        tag_start = key.end() 
         tag_end = tex.find('}',tag_start)
-        text_begin = tex.find('{',tag_end)
-        text_end = tex.find('}',text_begin)
+        text_begin = tex.find('[',tag_end)
+        text_end = tex.find(']',text_begin)
 
         fuzzy_tag = tex[tag_start:tag_end]
 
         highest = process.extractOne(fuzzy_tag,cite_keys)
-        ref_item = items[cite_keys.index(highest[0])]
+        print(highest)
+        ref_item = filtered_items[cite_keys.index(highest[0])]
+
+        print(fuzzy_tag, ref_item["title"], ref_item["itemID"])
+
+        formatted_reference = str(ref_item["itemID"])
+
+        #add url
+        if("DOI" in ref_item.keys()):
+            formatted_reference += " \href{https://doi.org/" + ref_item["DOI"] + "}{\\faExternalLink}"
+        elif("URL" in ref_item.keys()):
+            formatted_reference += " \href{" + ref_item["URL"] + "}{\\faExternalLink}"
+        else:
+            print("No url for item " + ref_item["title"])
 
 
-        formatted_reference = str(ref_item["itemID"]) + "\href{\faExternalLink}{+"  "+ }"
+        tex = tex[:text_begin+1] + formatted_reference + tex[text_end:]
 
-        tex = tex[:text_begin+1] +  + tex[text_end:]
+        # offset += len(formatted_reference) - (text_end-text_begin) + 1
+        # length of the inserted string minus the length of the string that was there
+        keys = list(re.finditer(CITE_TAG, tex)) # keys have moved
 
-        keys = re.finditer('supercite{', tex) # keys have moved
-
-
-    supercollections = re.finditer('supercollections{', tex)
-    for supercollection in supercollections:
-        tag_start = key.end()
-        tag_end = tex.find('}',tag_start)
-        text_begin = tex.find('{',tag_end)
-        text_end = tex.find('}',text_begin)
-
-        
+    #
+    # supercollections = re.finditer(COLLECT_TAG, tex)
+    # for supercollection in supercollections:
+    #     tag_start = supercollection.end()
+    #     tag_end = tex.find('}',tag_start)
+    #     text_begin = tex.find('{',tag_end)
+    #     text_end = tex.find('}',text_begin)
+    #
+    #     # "note"
+    #
+    #     keys = re.finditer(CITE_TAG, tex) # keys have moved
 
 
     with open(tex_file, "w+") as f:
