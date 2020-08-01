33e68397b7d435177f09d268a25b9bb55a3c941d
BasedOnTechnology <therobotist@gmail.com>
2020-08-01 18:22:35 -0400

    Noweb -> pweave

--- firmware/eppenwolf/protocols/noweb.sh
+++ firmware/eppenwolf/protocols/noweb.sh
@@ -2,7 +2,16 @@ filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-noweave -latex -delay $1 > $1.tex
+#git clone https://github.com/ifigueroap/noweb-minted
+#sudo ln -s /path/to/noweb-minted/noweb-minted /usr/local/bin
+#sudo ln -s /path/to/noweb-minted/guessPygmentizeLexer.py /usr/local/bin
+# bugs
+
+# git clone https://bitbucket.org/romildo/pygnoweb/
+# -filter $PWD/pygnoweb/pygnoweb.py
+
+
+noweave  -latex -delay $1 > $1.tex
 notangle -R$filename.py $1 > $1.py
 
 cp $1.tex ../../../documents/
