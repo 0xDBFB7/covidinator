30aedf4255dd7adf9835ddde916136e6a83b65c8
BasedOnTechnology <therobotist@gmail.com>
2020-08-18 19:00:09 -0400

    State

---
+++ electronics/simple_fdtd/fdtd_PCB_extensions/fdtd_PCB_extensions/tissue.py
@@ -0,0 +1,8 @@
+from .PCB import *
+
+
+def raw_voxels_to_numpy(filename):
+    np.frombuffer(s, dtype='int', count=5, offset=6)
+    
+
+# def import_raw(grid, filename, tissue_prop_filename, origin, ):
