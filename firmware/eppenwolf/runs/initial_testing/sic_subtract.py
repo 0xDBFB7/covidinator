cc242d6162d2d8432143359c1a47a0ddc43f585e
BasedOnTechnology <therobotist@gmail.com>
2020-08-28 23:27:20 -0400

    SiC test parameters

--- firmware/eppenwolf/runs/initial_testing/sic_subtract.py
+++ firmware/eppenwolf/runs/initial_testing/sic_subtract.py
@@ -5,15 +5,21 @@ from math import pi, sqrt, e, log, isclose, exp
 
 background = np.genfromtxt(open('sic_background_9.csv', "r"), delimiter=",", dtype=np.float, encoding='ascii')
 
-sic = np.genfromtxt(open('sic_9.csv', "r"), delimiter=",", dtype=np.float, encoding='ascii', skip_header=1)
+sic = np.genfromtxt(open('sic_9.csv', "r"), delimiter=",", dtype=np.float, encoding='ascii', skip_header=1, skip_footer=1)
+
+
+#
+# background = np.split(background, np.argwhere(background[:,0] == 11.95).flatten()[1:], axis=0)
+# sic = np.split(background, np.argwhere(sic[:,0] == 11.95).flatten()[1:], axis=0)
+
+# print(sic)
+# averaged_background = np.mean(background, 0)
+# averaged_sic = np.mean(sic, 0)
 
 print(background)
 print(sic)
 
-background = np.split(background, background[:,0] == 0)
-sic = np.split(sic, sic[:,0] == 0)
-
-plt.plot(sic[:,0], sic[:,1] - background[:,1])
-plt.plot(sic[:,0], sic[:,2] - background[:,2])
+plt.plot(averaged_sic[:,0], averaged_sic[:,1] - averaged_background[:,1])
+plt.plot(averaged_sic[:,0], averaged_sic[:,2] - averaged_background[:,2])
 
 plt.show()
