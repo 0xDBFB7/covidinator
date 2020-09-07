c7f4b6791fc275f6a269897e8605fd889e241adb
BasedOnTechnology <therobotist@gmail.com>
2020-09-07 19:39:42 -0400

    Generated slide

--- firmware/eppenwolf/runs/phage_experiment_4/generate.py
+++ firmware/eppenwolf/runs/phage_experiment_4/generate.py
@@ -1,10 +1,19 @@
-
+import pickle
 from numpy.random import choice
 
-
-
+candidate_names = ["Phage           ", "Autoclaved Phage", "Broth           "]
+candidates = [0,1,2]
 
 probabilities = [0.7, 0.2, 0.1]
 
-draw = choice(list_of_candidates, 8,
-              p=probabilities)
+choices = []
+
+for i in range(0, 100):
+    draw = choice(candidates, 8,
+                  p=probabilities)
+    choices.append(draw)
+    for i in draw:
+        print(candidate_names[i], end = "\t")
+    print("")
+
+pickle.dump(choices,open("choices.pkl", "wb" ))
