6502ecfb098ab1f5c2f65bf45d3108cc0dd53445
BasedOnTechnology <therobotist@gmail.com>
2020-10-08 21:42:17 -0400

    Protein charges processed

--- biology/data/net_charge.py
+++ biology/data/net_charge.py
@@ -1,7 +1,7 @@
 from localcider.sequenceParameters import SequenceParameters
 import glob
 #
- 
+
 # NOTE: Where pH values can be provided, if left blank we assume a neutral pH where only R/K/D/E are charged. If a pH value is provided, then R/K/D/E/C/Y/H are all considered titratable residues using EMBOSS pKa values, listed below: ‘C’: 8.5, ‘Y’: 10.1, ‘H’: 6.5, ‘E’: 4.1, ‘D’: 3.9, ‘K’: 10.0, ‘R’: 12.5
 
 files = glob.glob('protein_sequences/*.*')
@@ -10,11 +10,11 @@ for file in files:
     contents = open(file).read().split('>')
     #can't use SeqParam(seqfile=file)
     #because all the sequences are appended to each other.
-    output = open("charge" + file)
+    output = open(file + "_charge", 'w+')
     for protein in [x for x in contents if x]:
 
         header = protein[0:protein.index('\n')]
-        seq = protein[protein.index('\n'):-1] 
+        seq = protein[protein.index('\n'):-1]
 
         print(header)
         if('X' in seq):
@@ -28,7 +28,13 @@ for file in files:
         # net_charge_per_residue is alternating
         net_charge = seq_param.get_NCPR(pH=7.0)*seq_param.get_length()
         print(net_charge)
-    
-        output.write()
+
+        output.write(header)
+        output.write(", ")
+        output.write(str(7.0))
+        output.write(str(", "))
+        output.write(str(net_charge))
+        output.write('\n')
+
 
     output.close()
