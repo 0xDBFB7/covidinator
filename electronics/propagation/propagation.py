193fc8d9ba5add8a00e762b89ba82574d486449f
BasedOnTechnology <therobotist@gmail.com>
2020-11-26 00:08:40 -0500

    NLTL doesn't seem to help.

--- electronics/propagation/propagation.py
+++ electronics/propagation/propagation.py
@@ -7,3 +7,103 @@ from scipy.constants import epsilon_0, mu_0
 import scipy.constants
 from fdtd_PCB_extensions.tissue import cole_cole_4, complex_permittivity_to_er_and_sigma, electric_field_penetration_depth
 from mpl_toolkits.mplot3d import Axes3D
+from propagation_lib import propagator
+
+
+
+
+
+waveform = np.genfromtxt("NLTL_input_tone/NLTL-6275_cleaned.csv", delimiter=",", dtype=np.float, encoding='ascii', skip_header=0)
+
+waveform[:,0] = waveform[:,0] * 1e-12 #picoseonds
+
+input_frequency = 10e9
+sampling_frequency = 1000.0*input_frequency
+
+times = np.linspace(0, 1.0/input_frequency, 1000)
+input_data = np.interp(times, waveform[:,0], waveform[:,1])
+
+input_cycles = 10
+
+input_data = np.tile(input_data, input_cycles)
+
+
+input_data -= np.mean(input_data) #remove the stupid zero reject
+input_data /= np.max(input_data) # normalize
+
+
+p = propagator()
+p.fourier_transform(input_data, sampling_frequency, 1.0)
+
+
+depth_in_tissue = 0.001
+
+p.populate_tissue_properties(48) #muscle
+
+
+for idx, depth_in_tissue in enumerate(np.arange(0, 0.1, 0.001)):
+    p.fourier_transform(input_data, sampling_frequency, 1.0)
+    
+    spatial_phase = p.wavenumbers() * depth_in_tissue
+    # spatial_phase = np.zeros_like(p.mode_frequencies,dtype=np.float64)
+    p.attenuate(depth_in_tissue)
+
+    output = p.fourier_sum(spatial_phase)
+    output -= np.mean(output)
+    print(depth_in_tissue, 1/np.max(output))
+    # plt.plot(output[0:int(len(output)/(input_cycles/2))])
+plt.show()
+
+#assert(output - input == 0)
+
+# plt.plot(p.times, output)
+#        plt.plot(p.times, input_data)
+
+#Damping ratio: dimensionless number
+#Damping constant:
+
+osc_freq = 1000.0
+
+# k = 1e-7
+# charge = 1e7 * k
+
+
+#turn the E field into a force
+# p.a_n_coefficients *= charge
+# p.b_n_coefficients *= charge
+
+Q_factor = 100.0
+# damping_force_coefficient = #N s/m
+
+
+# mass = 120 * 1.66e-21 #kg
+effective_mass = 120 * 1.66e-21 #kg #fix!
+
+# beta = damping_force_coefficient/(2*effective_mass)
+# beta = (osc_freq * 2*pi) / (2.0*Q_factor)
+beta = 0.00000000000000001
+# beta =/
+
+# analytic_oscillator_scaling
+
+print(p.analytic_oscillator_scaling(0.1, 1.0))
+
+#beta is an exponential time constant, units /s.
+
+print(p.mode_frequencies)
+scale_factor = p.analytic_oscillator_scaling(beta, osc_freq)
+print(scale_factor)
+
+# plt.plot(p.mode_frequencies, p.a_n_coefficients)
+plt.plot(p.mode_frequencies, scale_factor*p.a_n_coefficients)
+plt.show()
+#
+p.a_n_coefficients *= scale_factor
+p.b_n_coefficients *= scale_factor
+osc_phase = p.analytic_oscillator_phase(beta, osc_freq)
+oscillator_output = p.fourier_sum(spatial_phase + osc_freq)
+#
+# # oscillator_output /= 1e-12
+#
+plt.plot(p.times, oscillator_output)
+plt.show()
--- electronics/propagation/propagation_lib.py
+++ electronics/propagation/propagation_lib.py
@@ -27,12 +27,12 @@ class propagator:
         self.b_n_coefficients = self.input_fft.imag
 
         frequency_step = (sampling_frequency * frequency_scale_factor)/len(self.a_n_coefficients)
-        
+
         self.times = np.linspace(0, len(self.a_n_coefficients)*(1.0/(sampling_frequency*frequency_scale_factor)), len(self.a_n_coefficients))
         self.mode_frequencies = np.linspace(frequency_step, (len(self.a_n_coefficients)/2)*frequency_step, len(self.a_n_coefficients))
 
 
-        
+
 
     def attenuate(self,distance):
         attenuation = np.exp(-distance/self.E_penetration_depth)
@@ -85,99 +85,138 @@ class propagator:
 
     def analytic_oscillator_phase(self, beta, resonant_frequency):
 
-        return np.arctan((2.0 * beta * 2*pi*self.mode_frequencies) / 
+        return -1.0 * np.arctan((2.0 * beta * 2*pi*self.mode_frequencies) /
             ((2*pi*resonant_frequency)**2.0 - (2*pi*self.mode_frequencies)**2.0))
         #angular frequencies?
-        
+
         #mode_freq is not correct
 
     def analytic_oscillator_scaling(self, beta, resonant_frequency):
         p1 = ((2*pi*resonant_frequency)**2.0 - (2*pi*self.mode_frequencies)**2.0)**2.0
         p2 = (4.0 * (beta**2.0) * (2*pi*self.mode_frequencies)**2.0)
+
+        #numpy uses weird normalization
+        # undo_normalization = len(self.mode_frequencies ** 2.0)
+
         return 1/np.sqrt(p1+p2)
-        
-    
+
+    # def dU_dx(U, t, E_field, charge):
+    #     # Here U is a vector such that y=U[0] and z=U[1]. This function should return [y', z']
+    #     k = *8.9e9
+    #     return [U[1], -2*U[1] - 2*U[0] + E_field*charge]
+
+
+
 class test_fourier(unittest.TestCase):
 
-    def test_sine_fourier(self):
-
-        sampling_frequency = 1000000
-        input_frequency = 1000.0
-        frequency_scale_factor = 1.0e6
-        times = np.linspace(0, 1.0, sampling_frequency)
-        input_data = np.zeros_like(times, dtype=np.float32)
-        input_data = np.sin(2.0*pi*times*1000.0)
-
-        p = propagator()
-        p.fourier_transform(input_data, sampling_frequency, frequency_scale_factor)
-
-        # plt.plot(p.mode_frequencies,p.a_n_coefficients)
-        # plt.show()
-        print(p.a_n_coefficients)
-        print(p.mode_frequencies)
-        print(times[-1])
+    # def test_sine_fourier(self):
+    #
+    #     sampling_frequency = 1000000
+    #     input_frequency = 1000.0
+    #     frequency_scale_factor = 1.0e6
+    #     times = np.linspace(0, 1.0, sampling_frequency)
+    #     input_data = np.zeros_like(times, dtype=np.float32)
+    #     input_data = np.sin(2.0*pi*times*1000.0)
+    #
+    #     p = propagator()
+    #     p.fourier_transform(input_data, sampling_frequency, frequency_scale_factor)
+    #
+    #     # plt.plot(p.mode_frequencies,p.a_n_coefficients)
+    #     # plt.show()
+    #     print(p.a_n_coefficients)
+    #     print(p.mode_frequencies)
+    #     print(times[-1])
 
 
 
     def test_fourier_reconstruction(self):
-        sampling_frequency = 50000
+        sampling_frequency = 500000.0
         input_frequency = 1000.0
-        frequency_scale_factor = 1e6
-        times = np.linspace(0, 1.0, sampling_frequency, dtype=np.float64)
+        frequency_scale_factor = 1
+        times = np.linspace(0, 1.0, int(sampling_frequency), dtype=np.float64)
         times = np.tile(times, 5)
         input_data = np.zeros_like(times, dtype=np.float64)
         #input_data[:500] = np.sin(2.0*pi*times*input_frequency)[0:500]
         #input_data = np.tile(input_data, 2)
         input_data = np.sin(2.0*pi*times*input_frequency)
-        
+
+
+        # filename = 'globalsave.pkl'
+        # try:
+        #     dill.load_session(filename)
+        # except:
+        #     os.system("rm dumps/*")
+        #     os.system("rm data/*")
+        #     voltages, currents, currents_2 = sim_VSWR(pcb)
+        #     dill.dump_session(filename)
 
         #plt.plot(input_data)
         #plt.show()
 
-        print(len(input_data))
         p = propagator()
         p.fourier_transform(input_data, sampling_frequency, frequency_scale_factor)
-        
-        p.populate_tissue_properties(39)
-        spatial_phase = p.wavenumbers() * 0.2
-        #spatial_phase = np.zeros_like(p.mode_frequencies,dtype=np.float32)
-        #p.attenuate(0.2)
 
-        
+        depth_in_tissue = 0.2
+
+        # p.populate_tissue_properties(48)
+        # spatial_phase = p.wavenumbers() * depth_in_tissue
+        spatial_phase = np.zeros_like(p.mode_frequencies,dtype=np.float64)
+        #p.attenuate(depth_in_tissue)
+
+
         output = p.fourier_sum(spatial_phase)
 
         #assert(output - input == 0)
 
-        plt.plot(p.times, output)
+        # plt.plot(p.times, output)
 #        plt.plot(p.times, input_data)
-        
+
         #Damping ratio: dimensionless number
         #Damping constant:
 
-        osc_freq = 1000e6
+        osc_freq = 1000.0
 
-        k = 1e-7
-        charge = 1e7  * k
-        
+        # k = 1e-7
+        # charge = 1e7 * k
+
 
         #turn the E field into a force
-        p.a_n_coefficients *= charge
-        p.b_n_coefficients *= charge
+        # p.a_n_coefficients *= charge
+        # p.b_n_coefficients *= charge
 
         Q_factor = 100.0
-        b = 1/(2*Q_factor)
+        # damping_force_coefficient = #N s/m
 
-        mass = 120 * 1.66e-21 #kg
-        
-        beta = b/(2*mass)
 
-        p.a_n_coefficients *= p.analytic_oscillator_scaling(beta, osc_freq)
-        p.b_n_coefficients *= p.analytic_oscillator_scaling(beta, osc_freq)
+        # mass = 120 * 1.66e-21 #kg
+        effective_mass = 120 * 1.66e-21 #kg #fix!
+
+        # beta = damping_force_coefficient/(2*effective_mass)
+        # beta = (osc_freq * 2*pi) / (2.0*Q_factor)
+        beta = 0.00000000000000001
+        # beta =/
+
+        # analytic_oscillator_scaling
+
+        print(p.analytic_oscillator_scaling(0.1, 1.0))
+
+        #beta is an exponential time constant, units /s.
+
+        print(p.mode_frequencies)
+        scale_factor = p.analytic_oscillator_scaling(beta, osc_freq)
+        print(scale_factor)
+
+        # plt.plot(p.mode_frequencies, p.a_n_coefficients)
+        plt.plot(p.mode_frequencies, scale_factor*p.a_n_coefficients)
+        plt.show()
+        #
+        p.a_n_coefficients *= scale_factor
+        p.b_n_coefficients *= scale_factor
         osc_phase = p.analytic_oscillator_phase(beta, osc_freq)
-        oscillator_output = p.fourier_sum(spatial_phase + osc_phase)
-
-        oscillator_output /= 1e-12
-
+        oscillator_output = p.fourier_sum(spatial_phase + osc_freq)
+        #
+        # # oscillator_output /= 1e-12
+        #
         plt.plot(p.times, oscillator_output)
         plt.show()
 
