ddbb360ccab01e3b53d3e9c28dd2dde50cefbfc7
BasedOnTechnology <therobotist@gmail.com>
2020-12-15 00:07:34 -0500

    Basinhopper yet again!

--- electronics/propagation/propagation_numerical_optimize.py
+++ electronics/propagation/propagation_numerical_optimize.py
@@ -7,45 +7,56 @@ from scipy.constants import epsilon_0, mu_0
 import scipy.constants
 from fdtd_PCB_extensions.tissue import cole_cole_4, complex_permittivity_to_er_and_sigma, electric_field_penetration_depth
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.optimize import minimize, basinhopping
 
-
-eps_inf = 4
-eps_s = 75
-tau_f = 5*10^-14
-tau = 8*10^-12
-omega_res = 8e9
+eps_inf = 4.0
+eps_s = 75.0
+tau_f = 5*10**-14
+tau = 8*10**-12
+omega_res = 2*pi*8e9
 beta = 1e-10
-Z0 = 377
+Z0 = 377.0
 c0 = 3e8
 m_reduced = 1
 q = 1
 
-F_optimal = subs(F_optimal, t, 0.0) #optimize time
-F_optimal = subs(F_optimal, z, 0.05)
+t= 0.0
+z = 0.05
 
 def greens_function(omega):
-    return 1.0/(omega_res**2 - (2*1j*beta*omega) - omega**2)
+    return 1.0/(omega_res**2 - (2.0*1j*beta*omega) - omega**2)
 
 def refractive_index(omega):
     #refractive index is n = sqrt(mu/epsilon)
-    return np.sqrt(eps_inf + (eps_s - eps_inf)/((1-1j*omega*tau)*(1-1j*omega*tau_f)))
+    return np.sqrt(eps_inf + (eps_s - eps_inf)/((1.0-1j*omega*tau)*(1.0-1j*omega*tau_f)))
 
 def propagate(F, omega, z):
 
     frequency_domain = np.fft.fft(F)
-
-    propagated = greens_function(omega) * q/m_reduced * (frequency_domain*exp(-1j*(omega/c0)*n(omega)*z))
+    # greens_function(omega) * q/m_reduced *
+    propagated = greens_function(omega) * (frequency_domain*np.exp(-1j*(omega/c0)*refractive_index(omega)*z))
     # watch the sign here - fix if needed
 
     return np.fft.ifft(propagated)
 
 
 def cost_function(F, omega, z):
-    return -np.max(propagate(F, omega, z)) + np.max(F)
+    return abs(-np.max(propagate(F, omega, z)) + np.max(F**2.0))
 
-duration = 10e-10
-samples = 100
+duration = 50e-10
+samples = 50
 times = np.linspace(-duration,duration,samples)
 
-F=ones(1,steps)
-omega = 2*pi*np.fft.fftfreq(N)*(samples/(duration*2))
+F=np.ones(samples)
+omega = 2*pi*np.fft.fftfreq(samples)*(samples/(duration*2))
+
+# output = propagate(F, omega, z)
+#
+
+# output = minimize(cost_function, F, args=(omega, z), options={'disp': True} )['x']
+
+output = basinhopping(cost_function, F, minimizer_kwargs={"args":(omega, z)}, disp=True)['x']
+
+
+plt.plot(times, output)
+plt.show()
