ddf29e61b13d7830787ffd5817d92a0a4c74abdb
BasedOnTechnology <therobotist@gmail.com>
2020-12-15 00:25:48 -0500

    Prop working, GF not yet

--- electronics/propagation/propagation_numerical_optimize.py
+++ electronics/propagation/propagation_numerical_optimize.py
@@ -14,14 +14,18 @@ eps_s = 75.0
 tau_f = 5*10**-14
 tau = 8*10**-12
 omega_res = 2*pi*8e9
-beta = 1e-10
+
 Z0 = 377.0
 c0 = 3e8
 m_reduced = 1
 q = 1
 
 t= 0.0
-z = 0.05
+z = 0.5
+
+
+Q = 2.0
+beta = 1
 
 def greens_function(omega):
     return 1.0/(omega_res**2 - (2.0*1j*beta*omega) - omega**2)
@@ -34,7 +38,7 @@ def propagate(F, omega, z):
 
     frequency_domain = np.fft.fft(F)
     # greens_function(omega) * q/m_reduced *
-    propagated = greens_function(omega) * (frequency_domain*np.exp(-1j*(omega/c0)*refractive_index(omega)*z))
+    propagated = (frequency_domain*np.exp(-1j*(omega/c0)*refractive_index(omega)*z))
     # watch the sign here - fix if needed
 
     return np.fft.ifft(propagated)
@@ -43,20 +47,23 @@ def propagate(F, omega, z):
 def cost_function(F, omega, z):
     return abs(-np.max(propagate(F, omega, z)) + np.max(F**2.0))
 
-duration = 50e-10
-samples = 50
+duration = 1000e-10
+samples = 300
 times = np.linspace(-duration,duration,samples)
 
 F=np.ones(samples)
 omega = 2*pi*np.fft.fftfreq(samples)*(samples/(duration*2))
+
+F[len(F)//2:-1] = 0
 
 # output = propagate(F, omega, z)
 #
 
 # output = minimize(cost_function, F, args=(omega, z), options={'disp': True} )['x']
 
-output = basinhopping(cost_function, F, minimizer_kwargs={"args":(omega, z)}, disp=True)['x']
-
+# output = basinhopping(cost_function, F, minimizer_kwargs={"method": "Nelder-Mead", "args":(omega, z)}, disp=True)['x']
+output = F
 
 plt.plot(times, output)
+plt.plot(times, propagate(output, omega, z))
 plt.show()
