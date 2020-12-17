b8fb5061b56e4722a1f5803bf96df8b1f6f2c3ff
BasedOnTechnology <therobotist@gmail.com>
2020-12-17 14:08:47 -0500

    Solvation's working well at this size scale

--- biology/simulation/GROMACS/BUMPy_bilayer/water_box.sh
+++ biology/simulation/GROMACS/BUMPy_bilayer/water_box.sh
@@ -3,4 +3,4 @@ GMX_DIR="/home/arthurdent/Programs/gromacs-2020.1/gromacs-2020.1/build/bin"
 
 #insane -x 100 -y 100 -z 100 -pbc cubic -sol W -o waterbox.pdb
 
-$GMX_DIR/gmx_mpi solvate -cp membrane.pdb -p topol.top -o solvated.gro -shell 1
+$GMX_DIR/gmx_mpi solvate -cp membrane.pdb -p topol.top -o solvated.pdb -shell 30
--- biology/simulation/GROMACS/BUMPy_bilayer/wrap.sh
+++ biology/simulation/GROMACS/BUMPy_bilayer/wrap.sh
@@ -1,5 +1,5 @@
 
-python3 bumpy.py -f bilayer.pdb -o membrane.pdb -n index.ndx -p topol.top -s sphere -z 200 -g r_sphere:100
+python3 bumpy.py -f bilayer.pdb -o membrane.pdb -n index.ndx -p topol.top -s sphere -z 0 -g r_sphere:100
 
 
 # There appears to be a bug in bumpy with Z values > 100
