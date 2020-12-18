64a1bb74a127e34996aeb1a9a020ee0ea46b936f
BasedOnTechnology <therobotist@gmail.com>
2020-12-18 18:17:45 -0500

    Minimizing again!

--- biology/simulation/GROMACS/BUMPy_bilayer/s2_lipid_only_minimize.sh
+++ biology/simulation/GROMACS/BUMPy_bilayer/s2_lipid_only_minimize.sh
@@ -1,13 +1,13 @@
 GMX_DIR="/home/arthurdent/Programs/gromacs-2020.1/gromacs-2020.1/build/bin"
 
 
-gmx genion -p topol.top
+#$GMX_DIR/gmx_mpi genion -p topol.top
 
 #gmx grompp -f polarize-water.mdp -r system-genion.gro -c system-genion.gro -p system.top -o system_minimization.tpr
 
-gmx grompp -f minimization.mdp -c 128_noW.gro -p dppc.top -o dppc-min-init.tpr
+$GMX_DIR/gmx_mpi grompp -f settings.mdp -c membrane.pdb -p topol.top -o bilayer.tpr
 
-gmx mdrun -deffnm dppc-min-init -v -c 128_minimized.gro
+#$GMX_DIR/gmx_mpi mdrun -deffnm dppc-min-init -v -c 128_minimized.gro
 
 #"Perform a short energy minimization of the system containing only the lipids;
 #the only reason for doing this, is getting rid of high forces
