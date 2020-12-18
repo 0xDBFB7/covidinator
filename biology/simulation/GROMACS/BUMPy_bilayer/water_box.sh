e585dc38f44f362aadb12666abbc8b65e0eac2c2
BasedOnTechnology <therobotist@gmail.com>
2020-12-17 23:15:23 -0500

    waater box

--- biology/simulation/GROMACS/BUMPy_bilayer/water_box.sh
+++ biology/simulation/GROMACS/BUMPy_bilayer/water_box.sh
@@ -3,6 +3,6 @@ GMX_DIR="/home/arthurdent/Programs/gromacs-2020.1/gromacs-2020.1/build/bin"
 
 #insane -x 10 -y 10 -z 10 -pbc cubic  -o waterbox.pdb
 
-$GMX_DIR/gmx_mpi editconf -box 30 -f membrane.pdb -o centered.pdb -bt cubic
+$GMX_DIR/gmx_mpi editconf -box 35 -f membrane.pdb -o centered.pdb -bt cubic
 
 $GMX_DIR/gmx_mpi solvate -cs polarize-water.gro -cp centered.pdb -p topol.top -o solvated.pdb -shell 30
