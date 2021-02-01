964712fd83b8dceae5573ab0c8ce26125c1372f5
BasedOnTechnology <therobotist@gmail.com>
2021-01-31 22:33:59 -0500

    :(

--- biology/simulation/poisson_boltzmann/pb.cpp
+++ biology/simulation/poisson_boltzmann/pb.cpp
@@ -1,28 +1,54 @@
 
+#include <cmath>
+#include "dbg.h"
+
+//g++ pb.cpp -o pb -fopenmp && ./pb
+
 
 #define base_type double
-
-void idx(x,y,z)
-
-
-base_type mesh[nx][ny][nz];
-base_type eps[nx][ny][nz];
-base_type pot[nx][ny][nz];
-base_type kappa_squared[nx][ny][nz];
+//
 
 
-for(int x = 1; x < nx-1; x++){
-    for(int y = 1; y < ny-1; y++){
-        for(int z = 1; z < nz-1; z++){
-            base_type value = 0;
-            value += -1.0*eps[x-1][y][z] * (pot[x-1][y][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z] * (pot[x+1][y][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y-1][z] * (pot[x][y-1][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z] * (pot[x][y+1][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z-1] * (pot[x][y][z-1]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z] * (pot[x][y][z+1]-pot[x][y][z]);
-            value += eps[x][y][z]*kappa_squared[x][y][z]*pot[x][y][z];
+const int n = 100;
+const int ncubed = (n*n*n);
+
+int idx(int x, int y, int z){
+  return ((n*n*z) + (n*y) + x);
+}
+
+base_type mesh[ncubed];
+base_type eps[ncubed];
+base_type pot[ncubed];
+base_type kappa_squared[ncubed];
+
+int main(){
+
+    #pragma omp parallel
+    {
+        for(int iter = 0; iter < 1000; iter++){
+            #pragma omp for
+            for(int x = 1; x < ncubed-1; x++){
+                for(int y = 1; y < ncubed-1; y++){
+                    for(int z = 1; z < ncubed-1; z++){
+                        base_type value = 0;
+                        value += -1.0*eps[x-1][y][z] * (pot[x-1][y][z]-pot[idx(x,y,z)]);
+                        value += -1.0*eps[idx(x,y,z)] * (pot[x+1][y][z]-pot[idx(x,y,z)]);
+                        value += -1.0*eps[x][y-1][z] * (pot[x][y-1][z]-pot[idx(x,y,z)]);
+                        value += -1.0*eps[idx(x,y,z)] * (pot[x][y+1][z]-pot[idx(x,y,z)]);
+                        value += -1.0*eps[x][y][z-1] * (pot[x][y][z-1]-pot[idx(x,y,z)]);
+                        value += -1.0*eps[idx(x,y,z)] * (pot[x][y][z+1]-pot[idx(x,y,z)];
+                        value -= eps[idx(x,y,z)]; //space charge term
+                        // value -= eps[idx(x,y,z)]*kappa_squared[idx(x,y,z)]*pot[idx(x,y,z)];
+
+                        value /= 6;
+                        pot[idx(x,y,z)] = value;
+                    }
+                }
+            }
+
         }
+        #pragma omp barrier
+        dbg(pot[0][0][0]);
     }
 }
 
--- biology/simulation/poisson_boltzmann/pb.py
+++ biology/simulation/poisson_boltzmann/pb.py
@@ -1,22 +1,32 @@
 
+import numpy as np
+import math
+
+import torch
 
 
-base_type mesh[nx][ny][nz];
-base_type eps[nx][ny][nz];
-base_type pot[nx][ny][nz];
-base_type kappa_squared[nx][ny][nz];
 
-for(int x = 1; x < nx-1; x++){
-    for(int y = 1; y < ny-1; y++){
-        for(int z = 1; z < nz-1; z++){
-            base_type value = 0;
-            value += -1.0*eps[x-1][y][z] * (pot[x-1][y][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z] * (pot[x+1][y][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y-1][z] * (pot[x][y-1][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z] * (pot[x][y+1][z]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z-1] * (pot[x][y][z-1]-pot[x][y][z]);
-            value += -1.0*eps[x][y][z] * (pot[x][y][z+1]-pot[x][y][z]);
-            value += eps[x][y][z]*kappa_squared[x][y][z]*pot[x][y][z];
-        }
-    }
-}
+dtype = torch.float
+#device = torch.device("cpu")
+device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+nx=100
+ny=100
+nz=100
+
+potential = torch.zeros((nx,ny,nz), device=device, dtype=dtype);
+eps = torch.ones((nx,ny,nz), device=device, dtype=dtype);
+kappa_squared = torch.zeros((nx,ny,nz));
+value = torch.zeros(1);
+
+for x in range(1,nx-1):
+    for y in range(1,ny-1):
+        for z in range(1,nz-1):
+            value = 0
+            value += -1.0*eps[x-1,y,z] * (potential[x-1,y,z]-potential[x,y,z]);
+            value += -1.0*eps[x,y,z] * (potential[x+1,y,z]-potential[x,y,z]);
+            value += -1.0*eps[x,y-1,z] * (potential[x,y-1,z]-potential[x,y,z]);
+            value += -1.0*eps[x,y,z] * (potential[x,y+1,z]-potential[x,y,z]);
+            value += -1.0*eps[x,y,z-1] * (potential[x,y,z-1]-potential[x,y,z]);
+            value += -1.0*eps[x,y,z] * (potential[x,y,z+1]-potential[x,y,z]);
+            value += eps[x,y,z]*kappa_squared[x,y,z]*potential[x,y,z];
